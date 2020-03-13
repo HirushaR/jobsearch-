049ef1165a91c5022588da8b581625c86a26f211
HirushaR <hirusharandunu11@gmail.com>
2020-03-13 14:23:41 +0530

    add the screen

---
+++ screen/jobs/jobsScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class jobsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> jobsScreen </Text>
+      </View>
+    );
+  }
+}
+
+export default jobsScreen;
---
+++ screen/users/userJobScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class userJobScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> userJobScreen </Text>
+      </View>
+    );
+  }
+}
+
+export default userJobScreen;
